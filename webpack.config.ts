849d5b87c9c6333dd12a672f480918209203f9ab
serghei madan <madan.serghei@yahoo.com>
2023-05-05 16:50:43 -0700

    added react and react-dom dependencies

--- webpack.config.ts
+++ webpack.config.ts
@@ -14,7 +14,7 @@ export default (env : BuildEnv) => {
     }
     
     const mode = env.mode || 'development'
-    const PORT = env.port || 3000
+    const PORT = 3001
     const isDev = mode === 'development'
     
     const config: webpack.Configuration = buildWebpackConfig({
