82ac84f16e8a46c7be38853f3b91a7d9dff946af
unknown <madan.serghei@yahoo.com>
2023-05-06 09:51:53 -0700

    added SCSS loader and css loader into webpack loaders

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+import "./Counter.scss";
+
 export const Counter = () => {
   const [count, setCount] = useState(0);
 
@@ -11,7 +13,7 @@ export const Counter = () => {
   };
 
   return (
-    <div>
+    <div className="counter">
       <p>Count: {count}</p>
       <button onClick={incrementCount}>increment</button>
       <button onClick={decrementCount}>decrement</button>
