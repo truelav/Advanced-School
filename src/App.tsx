5ba3a0bf78442b31f2a8b3f34d0bc45ba48c94ec
unknown <madan.serghei@yahoo.com>
2023-05-06 15:14:46 -0700

    created basic reset styles and global styles, also created basic dark and light theme styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
 import { Routes, Route, Link } from "react-router-dom";
 
-import "./index.scss";
-import { Suspense } from "react";
+import "./styles/index.scss";
+import { Suspense, useState } from "react";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 
+export enum Theme {
+  LIGHT = "light",
+  DARK = "dark",
+}
+
 export default function App() {
+  const [theme, setTheme] = useState<Theme>(Theme.LIGHT);
+
+  const toggleThem = () => {
+    setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK);
+  };
+
   return (
     <>
-      <div className="App">
+      <div className={`app ${theme}`}>
         App
+        <div>
+          <button onClick={toggleThem}>
+            Switch to {theme === "light" ? "Dark" : "Light"} Theme
+          </button>
+        </div>
         <Link to={"/"}>Main Page</Link>
         <Link to={"/about"}>Main Page</Link>
         <Suspense fallback={<div>Loading...</div>}>
