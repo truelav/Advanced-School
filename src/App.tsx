7b9f4d74d7c98ce5248772cc1708e7e55a381cdc
unknown <madan.serghei@yahoo.com>
2023-05-07 10:48:35 -0700

    added ClassNames helper function, it will conditionally join classNames together, this ways you can render or apply specific classNames based on the conditional rendering

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,13 @@ import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { ThemeContext, Theme } from "./theme/ThemeContext";
 import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 
 export default function App() {
   const { theme, toggleTheme } = useTheme();
   return (
     <>
-      <div className={`app ${theme}`}>
+      <div className={classNames("app", {}, [theme])}>
         App
         <div>
           <button onClick={toggleTheme}>
---
+++ src/helpers/classNames/classNames.ts
@@ -0,0 +1,19 @@
+type Mods = Record<string, boolean | string>;
+// Record is a special typescript object that will have the key as a string
+// and as a value a String or a Boolean value
+
+export function classNames(
+  cls: string,
+  mods: Mods,
+  additional: string[]
+): string {
+  return [
+    cls,
+    ...additional,
+    ...Object.entries(mods)
+      .filter(([className, value]) => Boolean(value))
+      .map(([className]) => className),
+  ].join(" ");
+}
+
+//example classNames('remove-btn', {hovered: true, selectable: true, red: false}, ['pdg'])
