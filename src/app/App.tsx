a895466d75d95e2fc6ac62bfa484435d05043359
serghei madan <madan.serghei@yahoo.com>
2023-05-08 10:21:54 -0700

    NAVBAR and APPLINK components, created APPLINK generic shared UI component, now we can use it instead of Basic LINK React component and pass through to='...' property, classnames function, and children

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,21 @@
-import { Routes, Route, Link } from "react-router-dom";
-
 import { useTheme } from "app/providers/ThemeProvider";
 import { classNames } from "shared/lib/classNames/classNames";
-import "./styles/index.scss";
 import { AppRouter } from "./providers/router";
+import { Navbar } from "widgets/Navbar";
+import "./styles/index.scss";
+
 
 export default function App() {
   const { theme, toggleTheme } = useTheme();
   return (
     <>
       <div className={classNames("app", {}, [theme])}>
-        App
+        <Navbar />
         <div>
           <button onClick={toggleTheme}>
             Switch to {theme === "light" ? "Dark" : "Light"} Theme
           </button>
         </div>
-        <Link to={"/"}>Main Page </Link>
-        <Link to={"/about"}> About Page</Link>
         <AppRouter />
       </div>
     </>
