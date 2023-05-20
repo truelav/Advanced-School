78f5da54e78fe95ee1d195eda621b3ca35319178
unknown <madan.serghei@yahoo.com>
2023-05-20 11:50:53 -0700

    added loki for the ui testing, but need to configure docker and run on docker

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,31 +1,27 @@
-import { classNames } from 'shared/lib/classNames/classNames';
-import { ButtonHTMLAttributes, FC } from 'react';
-import cls from './Button.module.scss';
+import { classNames } from "shared/lib/classNames/classNames";
+import { ButtonHTMLAttributes, FC } from "react";
+import cls from "./Button.module.scss";
 
 export enum ThemeButton {
-    CLEAR = 'clear',
+  CLEAR = "clear",
+  OUTLINE = "outline",
 }
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
-    className?: string;
-    theme?: ThemeButton;
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string;
+  theme?: ThemeButton;
 }
 
 export const Button: FC<ButtonProps> = (props) => {
-    const {
-        className,
-        children,
-        theme,
-        ...otherProps
-    } = props;
+  const { className, children, theme, ...otherProps } = props;
 
-    return (
-        <button
-            type="button"
-            className={classNames(cls.Button, { [cls[theme]]: true }, [className])}
-            {...otherProps}
-        >
-            {children}
-        </button>
-    );
+  return (
+    <button
+      type="button"
+      className={classNames(cls.Button, { [cls[theme]]: true }, [className])}
+      {...otherProps}
+    >
+      {children}
+    </button>
+  );
 };
