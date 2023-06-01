f9261bf59fe7744b40dd1d60e4538f57ee6152c7
serghei madan <madan.serghei@yahoo.com>
2023-06-01 10:31:47 -0700

    fixed the LoginModal

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,42 +1,40 @@
-import { useCallback, useState } from "react";
-import { Modal } from "shared/ui/Modal/Modal";
-import { classNames } from "shared/lib/classNames/classNames";
-import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
-import cls from "./Navbar.module.scss";
-import { Button, ButtonTheme } from "shared/ui/Button/Button";
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { Modal } from 'shared/ui/Modal/Modal';
+import React, { useCallback, useState } from 'react';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { LoginModal } from 'fetures/AuthByUsername';
+import cls from './Navbar.module.scss';
 
 interface NavbarProps {
-  className?: string;
+    className?: string;
 }
 
 export const Navbar = ({ className }: NavbarProps) => {
-  const [isAuthModal, setIsAuthModal] = useState(false);
+    const { t } = useTranslation();
+    const [isAuthModal, setIsAuthModal] = useState(false);
 
-  const onToggleModal = useCallback(() => {
-    setIsAuthModal((prev) => !prev);
-  }, []);
+    const onCloseModal = useCallback(() => {
+        setIsAuthModal(false);
+    }, []);
 
-  return (
-    <div className={classNames(cls.Navbar, {}, [className])}>
-      <div className={cls.links}>
-        {/* <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>
-          Главная
-        </AppLink>
-        <AppLink theme={AppLinkTheme.RED} to="/about">
-          О сайте
-        </AppLink> */}
-        <Button
-          onClick={onToggleModal}
-          className={cls.links}
-          theme={ButtonTheme.CLEAR_INVERTED}
-        >
-          Login
-        </Button>
-        <Modal isOpen={isAuthModal} onClose={onToggleModal}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-          alias a reiciendis inventore nostrum.
-        </Modal>
-      </div>
-    </div>
-  );
+    const onShowModal = useCallback(() => {
+        setIsAuthModal(true);
+    }, []);
+
+    return (
+        <div className={classNames(cls.Navbar, {}, [className])}>
+            <Button
+                theme={ButtonTheme.CLEAR_INVERTED}
+                className={cls.links}
+                onClick={onShowModal}
+            >
+                {t('Войти')}
+            </Button>
+            <LoginModal
+                isOpen={isAuthModal}
+                onClose={onCloseModal}
+            />
+        </div>
+    );
 };
