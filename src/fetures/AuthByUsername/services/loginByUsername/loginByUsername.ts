a86216a994dbea93ee5140c9915d65c013d20c1f
unknown <madan.serghei@yahoo.com>
2023-06-03 19:47:12 -0700

    addiung loginByUsername with createAsyncThunk

---
+++ src/fetures/AuthByUsername/services/loginByUsername/loginByUsername.ts
@@ -0,0 +1,25 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { User } from "entities/User";
+
+interface LoginByUsernameProps {
+  username: string;
+  password: string;
+}
+
+export const loginByUsername = createAsyncThunk<
+  User,
+  LoginByUsernameProps,
+  { rejectValue: string }
+>("login/loginByUsername", async (authData, thunkAPI) => {
+  try {
+    const response = await axios.post<User>("http://localhost:8000", authData);
+    if (!response.data) {
+      throw new Error();
+    }
+    return response.data;
+  } catch (e) {
+    console.log(e);
+    return thunkAPI.rejectWithValue("error happened");
+  }
+});
