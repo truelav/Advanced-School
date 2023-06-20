55898839df84f61b6e884dcba09265c2952133db
serghei madan <madan.serghei@yahoo.com>
2023-06-19 17:01:04 -0700

    loginByUsername test added

--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
@@ -1,4 +1,6 @@
+import { StateSchema } from "app/providers/StoreProvider";
 import axios from "axios";
+import { Dispatch } from "@reduxjs/toolkit";
 import { loginByUsername } from "./loginByUsername";
 
 jest.mock("axios");
@@ -6,10 +8,20 @@ jest.mock("axios");
 const mockedAxios = jest.mocked(axios, true);
 
 describe("loginByUsername.test", () => {
-  test("", () => {
+  let dispatch: Dispatch;
+  let getState: () => StateSchema
+
+  beforeEach(() => {
+    dispatch = jest.fn()
+    getState = jest.fn()
+  })
+
+  test("", async () => {
     mockedAxios.post.mockReturnValue(
       Promise.resolve({ data: { username: "123", id: "1" } })
     );
     const action = loginByUsername({ username: "123", password: "123" });
+    const result = await action(dispatch, getState, undefined)
+    console.log(result)
   });
 });
