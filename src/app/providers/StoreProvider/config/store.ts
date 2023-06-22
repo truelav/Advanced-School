6849c216c8d2b1bb9c18f608fa6b3c6f74e41a4a
unknown <madan.serghei@yahoo.com>
2023-06-22 11:17:10 -0700

    changed the server to ts, now it works fine and the login request also works fine

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,31 +1,48 @@
-import { configureStore, DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
-import { counterReducer } from 'entities/Counter';
-import { userReducer } from 'entities/User';
-import { StateSchema } from './StateSchema';
-import { createReducerManager } from './reducerManager';
+import {
+  configureStore,
+  DeepPartial,
+  ReducersMapObject,
+} from "@reduxjs/toolkit";
+import { counterReducer } from "entities/Counter";
+import { userReducer } from "entities/User";
+import { StateSchema } from "./StateSchema";
+import { createReducerManager } from "./reducerManager";
+import {
+  curryGetDefaultMiddleware,
+  getDefaultMiddleware,
+} from "@reduxjs/toolkit/dist/getDefaultMiddleware";
+import { $api } from "shared/api/api";
 
 export function createReduxStore(
-    initialState?: StateSchema,
-    asyncReducers?: ReducersMapObject<StateSchema>,
+  initialState?: StateSchema,
+  asyncReducers?: ReducersMapObject<StateSchema>
 ) {
-    const rootReducers: ReducersMapObject<StateSchema> = {
-        ...asyncReducers,
-        counter: counterReducer,
-        user: userReducer,
-    };
+  const rootReducers: ReducersMapObject<StateSchema> = {
+    ...asyncReducers,
+    counter: counterReducer,
+    user: userReducer,
+  };
 
-    const reducerManager = createReducerManager(rootReducers);
+  const reducerManager = createReducerManager(rootReducers);
 
-    const store = configureStore<StateSchema>({
-        reducer: reducerManager.reduce,
-        devTools: __IS_DEV__,
-        preloadedState: initialState,
-    });
+  const store = configureStore({
+    reducer: reducerManager.reduce,
+    devTools: __IS_DEV__,
+    preloadedState: initialState,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        thunk: {
+          extraArgument: {
+            api: $api,
+          },
+        },
+      }),
+  });
 
-    // @ts-ignore
-    store.reducerManager = reducerManager;
+  // @ts-ignore
+  store.reducerManager = reducerManager;
 
-    return store;
+  return store;
 }
 
-export type AppDispatch = ReturnType<typeof createReduxStore>['dispatch'];
+export type AppDispatch = ReturnType<typeof createReduxStore>["dispatch"];
--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
@@ -1,30 +1,42 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
-import { User, userActions } from 'entities/User';
-import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { User, userActions } from "entities/User";
+import { USER_LOCALSTORAGE_KEY } from "shared/const/localstorage";
 
 interface LoginByUsernameProps {
-    username: string;
-    password: string;
+  username: string;
+  password: string;
 }
 
-export const loginByUsername = createAsyncThunk<User, LoginByUsernameProps, { rejectValue: string }>(
-    'login/loginByUsername',
-    async (authData, thunkAPI) => {
-        try {
-            const response = await axios.post<User>('http://localhost:8000/login', authData);
+export const loginByUsername = createAsyncThunk<
+  User,
+  LoginByUsernameProps,
+  { rejectValue: string }
+>(
+  "login/loginByUsername",
+  async (authData, { extra, dispatch, rejectWithValue }) => {
+    try {
+      // const response = await axios.post<User>('http://localhost:8000/login', authData);
+      // @ts-ignore
+      const response = await extra.api.post<User>(
+        "http://localhost:8000/login",
+        authData
+      );
 
-            if (!response.data) {
-                throw new Error();
-            }
+      if (!response.data) {
+        throw new Error();
+      }
 
-            localStorage.setItem(USER_LOCALSTORAGE_KEY, JSON.stringify(response.data));
-            thunkAPI.dispatch(userActions.setAuthData(response.data));
+      localStorage.setItem(
+        USER_LOCALSTORAGE_KEY,
+        JSON.stringify(response.data)
+      );
+      dispatch(userActions.setAuthData(response.data));
 
-            return response.data;
-        } catch (e) {
-            console.log(e);
-            return thunkAPI.rejectWithValue('error');
-        }
-    },
+      return response.data;
+    } catch (e) {
+      console.log(e);
+      return rejectWithValue("error");
+    }
+  }
 );
