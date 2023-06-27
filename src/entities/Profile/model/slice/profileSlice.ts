4dd7b80a81e119804b23332036b8acfa4f2da5a3
unknown <madan.serghei@yahoo.com>
2023-06-27 10:41:49 -0700

    added ProfileCard ui entity

--- src/entities/Profile/model/slice/profileSlice.ts
+++ src/entities/Profile/model/slice/profileSlice.ts
@@ -1,32 +1,36 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { fetchProfileData } from '../services/fetchProfileData/fetchProfileData';
-import { ProfileSchema } from '../types/profile';
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { fetchProfileData } from "../services/fetchProfileData/fetchProfileData";
+import { Profile, ProfileSchema } from "../types/profile";
 
 const initialState: ProfileSchema = {
-    readonly: true,
-    isLoading: false,
-    error: undefined,
-    data: undefined,
+  readonly: true,
+  isLoading: false,
+  error: undefined,
+  data: undefined,
 };
 
 export const profileSlice = createSlice({
-    name: 'profile',
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => {
-        builder
-            .addCase(fetchProfileData.pending, (state) => {
-                state.error = undefined;
-                state.isLoading = true;
-            })
-            .addCase(fetchProfileData.fulfilled, (state) => {
-                state.isLoading = false;
-            })
-            .addCase(fetchProfileData.rejected, (state, action) => {
-                state.isLoading = false;
-                state.error = action.payload;
-            });
-    },
+  name: "profile",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProfileData.pending, (state) => {
+        state.error = undefined;
+        state.isLoading = true;
+      })
+      .addCase(
+        fetchProfileData.fulfilled,
+        (state, action: PayloadAction<Profile>) => {
+          state.isLoading = false;
+          state.data = action.payload;
+        }
+      )
+      .addCase(fetchProfileData.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      });
+  },
 });
 
 // Action creators are generated for each case reducer function
