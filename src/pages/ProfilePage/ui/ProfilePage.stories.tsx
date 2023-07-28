14bc00307e5f8939ba6271f4d3729e482b3efe0b
serghei madan <madan.serghei@yahoo.com>
2023-07-28 16:57:11 -0700

    fetchProfileData.test  thunk.api not working

--- src/pages/ProfilePage/ui/ProfilePage.stories.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.stories.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
 import ProfilePage from 'pages/ProfilePage/ui/ProfilePage';
 import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
+import { Currency } from 'entities/Currency';
+import { Country } from 'entities/Country';
 
 export default {
     title: 'pages/ProfilePage',
@@ -17,7 +18,19 @@ const Template: ComponentStory<typeof ProfilePage> = (args) => <ProfilePage {...
 
 export const Normal = Template.bind({});
 Normal.args = {};
-Normal.decorators = [StoreDecorator({})];
+Normal.decorators = [StoreDecorator({
+    profile: {
+        form: {
+            username: 'admin',
+            age: 22,
+            country: Country.Ukraine,
+            lastname: 'mynameis',
+            first: 'serghei',
+            city: 'Kiev',
+            currency: Currency.USD,
+        }
+    }
+})];
 
 export const Dark = Template.bind({});
 Dark.args = {};
