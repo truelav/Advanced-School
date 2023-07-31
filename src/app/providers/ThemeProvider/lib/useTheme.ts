64851c64c3de1c1a38cc92961fed0197942cc2b7
unknown <madan.serghei@yahoo.com>
2023-07-30 17:10:25 -0700

    added concurrently, now wen run stard:dev it will start the frontend and the server

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -1,22 +1,38 @@
-import { useContext } from 'react';
-import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from './ThemeContext';
+import { useContext } from "react";
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from "./ThemeContext";
 
 interface UseThemeResult {
-    toggleTheme: () => void;
-    theme: Theme;
+  toggleTheme: () => void;
+  theme: Theme;
 }
 
 export function useTheme(): UseThemeResult {
-    const { theme, setTheme } = useContext(ThemeContext);
+  const { theme, setTheme } = useContext(ThemeContext);
 
-    const toggleTheme = () => {
-        const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
-        setTheme(newTheme);
-        localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
-    };
+  const toggleTheme = () => {
+    let newTheme: Theme;
 
-    return {
-        theme,
-        toggleTheme,
-    };
+    switch (theme) {
+      case Theme.DARK:
+        newTheme = Theme.DARK;
+        break;
+      case Theme.LIGHT:
+        newTheme = Theme.LIGHT;
+        break;
+      case Theme.ORANGE:
+        newTheme = Theme.ORANGE;
+        break;
+      default:
+        newTheme = Theme.LIGHT;
+    }
+
+    setTheme?.(newTheme);
+    console.log(theme, newTheme);
+    localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
+  };
+
+  return {
+    theme,
+    toggleTheme,
+  };
 }
