013e404934ce37ac8cedb194c7483c9b1ffefeab
serghei madan <madan.serghei@yahoo.com>
2023-08-16 16:27:17 -0700

    adding services

---
+++ src/entities/Article/model/services/fetchArticleById.ts
@@ -0,0 +1,18 @@
+import { createAsyncThunk, isRejectedWithValue } from "@reduxjs/toolkit";
+import { AxiosInstance } from "axios";
+import { ThunkConfig } from "app/providers/StoreProvider";
+import { Article } from "../types/article";
+
+export const fetchArticleById = createAsyncThunk<Article, string, ThunkConfig<string>>('articleDetails/fetchArticleById', async (articleId, thunkApi) => {
+    const { extra, rejectWithValue } = thunkApi
+    try {
+        const response = await extra.api.get<Article>('/articles/')
+
+        if(!response.data){
+            throw new Error()
+        }
+    } catch(e){
+        console.log(e)
+        return rejectWithValue("error")
+    }
+})
