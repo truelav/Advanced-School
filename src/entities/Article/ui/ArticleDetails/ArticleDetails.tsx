dcf260a682b36b138dd6755e854f6433126cb3b2
unknown <madan.serghei@yahoo.com>
2023-08-17 12:25:57 -0700

    added content for ArticleDetails, data, isLoading and Error

--- src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
+++ src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
@@ -8,6 +8,12 @@ import cls from "./ArticleDetails.module.scss";
 import { useEffect, memo } from "react";
 import { useAppDispatch } from "shared/lib/hooks/useAppDispatch/useAppDispatch";
 import { fetchArticleById } from "entities/Article/model/services/fetchArticleById";
+import { useSelector } from "react-redux";
+import {
+  getArticleDetails,
+  getArticleDetailsError,
+  getArticleDetailsIsLoading,
+} from "entities/Article/model/selectors/articleDetails";
 
 interface ArticleDetailsProps {
   className?: string;
@@ -21,15 +27,27 @@ const reducers: ReducersList = {
 export const ArticleDetails = memo((props: ArticleDetailsProps) => {
   const { className, id } = props;
   const dispatch = useAppDispatch();
+  const article = useSelector(getArticleDetails);
+  const isLoading = useSelector(getArticleDetailsIsLoading);
+  const error = useSelector(getArticleDetailsError);
+  let content;
 
   useEffect(() => {
     dispatch(fetchArticleById(id));
   }, [dispatch, id]);
 
+  if (isLoading) {
+    content = <div>Loading...</div>;
+  } else if (error) {
+    content = <div>Error Happened</div>;
+  } else {
+    content = <div> Article {id} Details </div>;
+  }
+
   return (
     <DynamicModuleLoader reducers={reducers} removeAfterUnmount={true}>
       <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
-        Article Details
+        {content}
       </div>
     </DynamicModuleLoader>
   );
