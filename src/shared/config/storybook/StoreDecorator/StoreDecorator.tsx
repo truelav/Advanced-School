65d46f1cb84ad99423fecf297cdadd633084d2cf
unknown <madan.serghei@yahoo.com>
2023-08-22 22:14:03 -0700

    store decorator added Article

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -1,19 +1,27 @@
-import { Story } from '@storybook/react';
-import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
-import { DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
-import { loginReducer } from 'features/AuthByUsername/model/slice/loginSlice';
-import { profileReducer } from 'entities/Profile';
+import { Story } from "@storybook/react";
+import { StateSchema, StoreProvider } from "app/providers/StoreProvider";
+import { DeepPartial, ReducersMapObject } from "@reduxjs/toolkit";
+import { loginReducer } from "features/AuthByUsername/model/slice/loginSlice";
+import { profileReducer } from "entities/Profile";
+import { articleDetailsReducer } from "entities/Article/model/slice/articleDetailsSlice";
 
 const defaultAsyncReducers: DeepPartial<ReducersMapObject<StateSchema>> = {
-    loginForm: loginReducer,
-    profile: profileReducer,
+  loginForm: loginReducer,
+  profile: profileReducer,
+  articleDetails: articleDetailsReducer,
 };
 
-export const StoreDecorator = (
+export const StoreDecorator =
+  (
     state: DeepPartial<StateSchema>,
-    asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>,
-) => (StoryComponent: Story) => (
-    <StoreProvider initialState={state} asyncReducers={{ ...defaultAsyncReducers, ...asyncReducers }}>
+    asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>
+  ) =>
+  (StoryComponent: Story) =>
+    (
+      <StoreProvider
+        initialState={state}
+        asyncReducers={{ ...defaultAsyncReducers, ...asyncReducers }}
+      >
         <StoryComponent />
-    </StoreProvider>
-);
+      </StoreProvider>
+    );
