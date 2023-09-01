4582781b1ed6db7742714a33aa892a3c88474250
serghei madan <madan.serghei@yahoo.com>
2023-09-01 15:14:25 -0700

    fixed key missing error and removed i18 dev setting

--- src/entities/Article/ui/ArticleViewSelector/ArticleViewSelector.tsx
+++ src/entities/Article/ui/ArticleViewSelector/ArticleViewSelector.tsx
@@ -33,8 +33,9 @@ export const ArticleViewSelector = memo((props: ArticleViewSelectorProps) => {
 
     return (
         <div className={classNames(cls.ArticleViewSelector, {}, [className])}>
-            {viewTypes.map((viewType) => (
+            {viewTypes.map((viewType, idx) => (
                 <Button
+                    key={`` + idx + view}
                     theme={ButtonTheme.CLEAR}
                     onClick={onClick(viewType.view)}
                 >
