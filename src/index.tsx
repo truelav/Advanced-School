59afb9fc12629fc8293af58b5645b77b484437ee
truelav <madan.serghei@yahoo.com>
2024-03-22 16:58:48 -0700

    buildWebpackConfig - removed unused imports and variables

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
-import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from 'app/providers/ThemeProvider';
-import { StoreProvider } from 'app/providers/StoreProvider';
-import App from './app/App';
-import 'app/styles/index.scss';
-import './shared/config/i18n/i18n';
-import { ErrorBoundary } from './app/providers/ErrorBoundary';
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "app/providers/ThemeProvider";
+import { StoreProvider } from "app/providers/StoreProvider";
+import App from "./app/App";
+import "app/styles/index.scss";
+import "./shared/config/i18n/i18n";
+import { ErrorBoundary } from "./app/providers/ErrorBoundary";
 
 render(
     <BrowserRouter>
-    <   StoreProvider>
+        <StoreProvider>
             <ErrorBoundary>
                 <ThemeProvider>
                     <App />
@@ -17,5 +17,5 @@ render(
             </ErrorBoundary>
         </StoreProvider>
     </BrowserRouter>,
-    document.getElementById('root'),
+    document.getElementById("root")
 );
